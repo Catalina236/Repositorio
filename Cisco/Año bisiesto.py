fb8ea85fd5b823512809f8b68775c050ed14e3d8
Catalina <116193295+Catalina236@users.noreply.github.com>
2023-02-02 19:25:12 -0500

    Delete Cisco directory

--- Cisco/Año bisiesto.py
+++
@@ -1,8 +0,0 @@
-year=int(input("Ingrese año: "))
-if year<=1582:
-    print("No está dentro del período del calendario gregoriano")
-elif year%4!=0:
-    print("Es un año común")
-else:
-    print("Es un año bisiesto")
-    
--- Cisco/Ejercicio del espatifilio.py
+++
@@ -1,7 +0,0 @@
-Planta=str(input("Ingrese planta: "))
-if Planta=="Espatifilio":
-    print("¡Espatifilio es la mejor planta de todas!")
-elif Planta=="pelargonio":
-   print("!ESPATIFILIO!, ¡No pelargonio!")
-else:
-    print("No, ¡quiero un gran ESPATIFILIO!")
