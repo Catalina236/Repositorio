b8b89bd15832b07defad9e619951fed816ae42bd
Catalina <116193295+Catalina236@users.noreply.github.com>
2023-02-20 08:59:40 -0500

    Add files via upload

---
+++ Cadenas/Ejercicio2.py
@@ -0,0 +1,7 @@
+def suma():
+    palabra=input("Ingrese una palabra: ")
+    for i in range(len(palabra)):
+        for j in range(i):
+            i+=j
+    print("El valor de la suma de todos los códigos de la palabra es: ",i)
+suma()
