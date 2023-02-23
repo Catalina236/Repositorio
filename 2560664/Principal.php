e79ffbcdcca6d630d1bab4615e63a6480954c0c9
Catalina <116193295+Catalina236@users.noreply.github.com>
2023-02-22 23:48:08 -0500

    Add files via upload

---
+++ 2560664/Principal.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Estilos/Estilos.css">
+    <title>Cuentos</title>
+</head>
+<body>
+    <header>
+        <h1>La sirenita</h1>
+        <p>
+        <br>
+        En medio del mar, en las más grandes profundidades, se extendía un reino mágico, el reino del pueblo del mar. Un lugar de extraordinaria belleza rodeado por flores y plantas únicas y en el que se encontraba el castillo del rey del mar.
+        <br>
+        <br>
+        Él y sus seis hijas vivían felices en medio de tanta belleza. Ellas pasaban el día jugando y cuidando de sus flores en los majestuosos jardines de árboles azules y rojos. La más pequeña de ellas, era la más especial. Su piel era blanca y suave, sus ojos grandes y azules, pero como el resto de las sirenas, tenía cola de pez. A la pequeña sirena le fascinaban las historias que su abuela contaba acerca de los seres humanos, tanto que cuando encontró una estatua de un hombre en los restos de un barco que naufragó no se lo pensó y se la llevó para ponerla en su jardín. La abuela les contó que algún día conocerían la superficie.
+        <br>
+        <br>
+        - Cuando cumpláis quince años podréis subir a la superficie y podréis contemplar los bosques, las ciudades y todo lo que hay allí. Hasta entonces está prohibido.
+        <br>
+        <br>
+        La pequeña sirena esperó a que llegara su turno ansiosa, imaginando como sería el mundo de allá arriba. Cada vez que a una de sus hermanas le llegaba el turno y cumplía los quince años, ella escuchaba atentamente las cosas que contaba y eso aumentaba sus ganas porque llegara el momento de subir.
+        <br>
+        <br>
+        Tras años de espera por fin cumplió quince años. La sirena subió y se encontró con un gran barco en el que celebraban una fiesta. Oía música y alboroto y no pudo evitar acercarse para tratar de ver a través de una de sus ventanas. Entre la gente distinguió a un joven apuesto, que resultó ser el príncipe, y por quien quedó embelesada al observar su belleza.
+        </p>
+
+        <br>
+        <br>
+        <div class="cab_izq">
+            <div class="caja_izq">
+                <img src="Imágenes/sirenita.jpg" alt="">
+            </div>
+        </div>
+        <div class="cuadro"></div>
+        <div class="segundo_cuadro"></div>
+    </header>
+    
+</body>
+</html>
